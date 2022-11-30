8c533a23b5fa95efb6827c4f1cbd3f2bf598eeed
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-30 18:10:54 +0200

    add directives

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import { store } from './store/store.js'
 import './assets/styles/setup/_typography.scss'
 import './assets/styles/main.scss'
 import rootCmp from './root-cmp.vue'
+import { clickOutsideDirective, focusDirective, rainbowDirective } from './directives'
 
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import './font-awesome'
@@ -13,6 +14,9 @@ import './font-awesome'
 const app = createApp(rootCmp)
 
 app.component('font-awesome-icon', FontAwesomeIcon) 
+app.directive('outside-click', clickOutsideDirective)
+app.directive('focus', focusDirective)
+app.directive('rainbow', rainbowDirective)
 app.use(router)
 app.use(store)
 
