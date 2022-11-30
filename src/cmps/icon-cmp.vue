81515b58210b86f771081ce0f721fefd7a572274
Miriam Smith <miriamzeff@gmail.com>
2022-11-30 17:59:57 +0200

    cleaned up icon cmp

--- src/cmps/icon-cmp.vue
+++ src/cmps/icon-cmp.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script>
-// import {userService} from '../services/user.service'
 
 export default {
   props: {
@@ -12,48 +11,14 @@ export default {
   data() {
     return {
       type: {
-        star: '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 14px; width: 14px; fill: currentcolor;"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" fill-rule="evenodd"></path></svg>',
-        search: '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>',
+        star: 
+          '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 14px; width: 14px; fill: currentcolor;"><path d="M15.094 1.579l-4.124 8.885-9.86 1.27a1 1 0 0 0-.542 1.736l7.293 6.565-1.965 9.852a1 1 0 0 0 1.483 1.061L16 25.951l8.625 4.997a1 1 0 0 0 1.482-1.06l-1.965-9.853 7.293-6.565a1 1 0 0 0-.541-1.735l-9.86-1.271-4.127-8.885a1 1 0 0 0-1.814 0z" fill-rule="evenodd"></path></svg>',
+        search:
+          '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 4; overflow: visible;"><g fill="none"><path d="m13 24c6.0751322 0 11-4.9248678 11-11 0-6.07513225-4.9248678-11-11-11-6.07513225 0-11 4.92486775-11 11 0 6.0751322 4.92486775 11 11 11zm8-3 9 9"></path></g></svg>',
+        heart:
+          '<svg viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; fill: none; height: 16px; width: 16px; stroke: currentcolor; stroke-width: 2; overflow: visible;"><path d="m16 28c7-4.733 14-10 14-17 0-1.792-.683-3.583-2.05-4.95-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05l-2.051 2.051-2.05-2.051c-1.367-1.366-3.158-2.05-4.95-2.05-1.791 0-3.583.684-4.949 2.05-1.367 1.367-2.051 3.158-2.051 4.95 0 7 7 12.267 14 17z"></path></svg>',
       },
     }
-  },
-  async created() {
-    // const user = await userService.getById(id)
-    let stayId = this.$route.params.id
-    this.getStayById(stayId)
-    console.log('icon type:', this.iconType);
-  },
-  // watch: {
-  //   stayId: {
-  //     handler() {
-  //       if(this.stayId){
-  //           this.$store.dispatch({ type: "loadAndWatchUser", userId: this.userId })
-  //       }
-  //     },
-  //     immediate: true,
-  //   },
-  // },
-  computed: {
-    // stay() {
-    //   return this.$store.getters.watchedUser
-    // },
-    avgRating() {
-      return (
-        this.stay.reviews.reduce((r1, r2) => r1.rate + r2.rate, 0) /
-        this.countReviews
-      )
-    },
-    countReviews() {
-      return this.stay.reviews.length
-    },
-  },
-  methods: {
-    async getStayById(stayId) {
-      this.stay = await this.$store.dispatch({
-        type: 'getStayById',
-        stayId,
-      })
-    },
   },
 }
 </script>
