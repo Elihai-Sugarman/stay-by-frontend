45bbd9ee2350a8300b2849c35da6438635f0ed1f
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-30 16:10:48 +0200

    add icons

--- src/font-awesome.js
+++ src/font-awesome.js
@@ -1,4 +1,11 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons'
+import { faMagnifyingGlass, faUser, faBars } from '@fortawesome/free-solid-svg-icons'
+// import { fabar } from '@fortawesome/free-regular-svg-icons'
 
-library.add(faAirbnb)
+library.add(
+  faAirbnb,
+  faMagnifyingGlass,
+  faUser,
+  faBars
+)
