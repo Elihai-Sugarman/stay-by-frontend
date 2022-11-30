41e89dba7802b9da84c0fd17d25de2dc3860daf2
Miriam Smith <miriamzeff@gmail.com>
2022-11-30 21:18:18 +0200

    sstay details update

---
+++ src/cmps/stay/stay-amenity.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+    {{camelizedAmenity}}
+    <icon-cmp :iconType="camelizedAmenity" />
+    <span>{{amenity}}</span>
+  </section>
+</template>
+
+<script>
+import iconCmp from '../icon-cmp.vue'
+
+export default {
+  props: {
+    amenity: String,
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    camelizedAmenity() {
+      return this.amenity
+        .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+          return index === 0 ? word.toLowerCase() : word.toUpperCase()
+        })
+        .replace(/\s+/g, '')
+    },
+  },
+  components: {
+    iconCmp
+  }
+}
+</script>
--- src/views/stay/stay-details.vue
+++ src/views/stay/stay-details.vue
@@ -1,19 +1,118 @@
 <template>
   <section v-if="stay">
-    <h1>{{ stay.name }}</h1>
-    <div class="stay-details-subtitle">
-      <font-awesome-icon icon="fa-solid fa-star" />
+    <h1 class="name-title">{{ stay.name }}</h1>
+    <div class="name-subtitle">
+      <span class="avg-rating">
+        <!-- <font-awesome-icon icon="fa-solid fa-star" /> -->
+        <icon-cmp iconType="star" />
+        {{ avgRating }}
+      </span>
+      <span class="rev-count">{{ countReviews }} reviews</span>
+      <span class="superhost" v-if="stay.host.isSuperhost">
+        <icon-cmp iconType="bwBadge" />
+        Superhost
+      </span>
+      <span class="address">
+        {{ stay.address.street }}, {{ stay.address.country }}
+      </span>
     </div>
-    <!-- <h3>{{ user.username }} score: {{ user.score }}</h3>
-      <img style="max-width: 200px;" :src="user.imgUrl" />
-      <ul>
-        <li v-for="review in user.givenReviews" :key="review._id">
-          {{ review.txt }}
-          <router-link :to="`/user/${review.aboutUser._id}`">
-            About {{ review.aboutUser.fullname }}
-          </router-link>
-        </li>
-      </ul> -->
+    <div class="images-container">
+      <li v-for="img in imgsToDisplay">
+        <img :src="img" />
+      </li>
+    </div>
+
+    <div class="more-details">
+      <div class="heading">
+        <h2 class="title">
+          {{ stay.type }} hosted by {{ stay.host.fullname }}
+        </h2>
+        <div class="subtitle">{{ stay.capacity }} guests</div>
+        <img :src="stay.host.imgUrl" />
+      </div>
+
+      <div class="special-perks">
+        <div v-if="stay.host.isSuperhost">
+          <icon-cmp iconType="bwBadge" />
+          <h4>{{ stay.host.fullname }} is a Superhost</h4>
+          <span
+            >Superhosts are experienced, highly rated hosts who are committed to
+            providing great stays for guests.</span
+          >
+        </div>
+
+        <div>
+          <icon-cmp iconType="key" />
+          <h4>Great check-in experience</h4>
+          <span
+            >100% of recent guests gave the check-in process a 5-star
+            rating.</span
+          >
+        </div>
+
+        <div>
+          <icon-cmp iconType="locMarker" />
+          <h4>Great location</h4>
+          <span>90% of recent guests gave the location a 5-star rating.</span>
+        </div>
+
+        <div>
+          <icon-cmp iconType="greatComm" />
+          <h4>Great communication</h4>
+          <span
+            >100% of recent guests rated {{ stay.host.fullname }} 5-star in
+            communication.</span
+          >
+        </div>
+
+        <div>
+          {{ stay.summary }}
+        </div>
+
+        <div class="amenities">
+          <h4>What this place offers</h4>
+
+          <div>
+            <li v-for="amenity in stay.amenities">
+              <stayAmenity :amenity="amenity"/>
+            </li>
+          </div>
+
+          <div v-if="stay.amenities.includes('Heating')">
+            <icon-cmp iconType="thermometer" />
+            <span>Heating</span>
+          </div>
+          <div v-if="stay.amenities.includes('Bathroom essentials')">
+            <icon-cmp iconType="bathEssentials" />
+            <span>Bathroom essentials</span>
+          </div>
+          <div v-if="stay.amenities.includes('Cooking basics')">
+            <icon-cmp iconType="kitchen" />
+            <span>Cooking basics</span>
+          </div>
+          <div v-if="stay.amenities.includes('Bed linens')">
+            <icon-cmp iconType="linens" />
+            <span>Bed linens</span>
+          </div>
+          <div v-if="stay.amenities.includes('Hot water kettle')">
+            <icon-cmp iconType="kettle" />
+            <span>Hot water kettle</span>
+          </div>
+          <div v-if="stay.amenities.includes('Dishes and silverware')">
+            <icon-cmp iconType="dishes" />
+            <span>Dishes and silverware</span>
+          </div>
+          <div v-if="stay.amenities.includes('Washer')">
+            <icon-cmp iconType="washer" />
+            <span>Washer</span>
+          </div>
+          <div v-if="stay.amenities.includes('Dishwasher')">
+            <icon-cmp iconType="dishwasher" />
+            <span>Dishwasher</span>
+          </div>
+        </div>
+      </div>
+    </div>
 
     <details>
       <summary>Full JSON</summary>
@@ -24,6 +123,8 @@
 
 <script>
 // import {userService} from '../services/user.service'
+import iconCmp from '../../cmps/icon-cmp.vue'
+import stayAmenity from '../../cmps/stay/stay-amenity.vue'
 
 export default {
   data() {
@@ -47,9 +148,19 @@ export default {
   //   },
   // },
   computed: {
-    // stay() {
-    //   return this.$store.getters.watchedUser
-    // },
+    avgRating() {
+      return +(
+        this.stay.reviews.reduce((acc, currRev) => acc + currRev.rate, 0) /
+        this.countReviews
+      ).toFixed(2)
+    },
+    countReviews() {
+      return this.stay.reviews.length
+    },
+    imgsToDisplay() {
+      // return this.stay.imgUrls.concat(this.stay.imgUrls.slice(0,2))
+      return [...this.stay.imgUrls, ...this.stay.imgUrls.slice(0, 2)]
+    },
   },
   methods: {
     async getStayById(stayId) {
@@ -58,6 +169,10 @@ export default {
         stayId,
       })
     },
+  },
+  components: {
+    iconCmp,
+    stayAmenity
   },
 }
 </script>
