1b329e132a22b39ab57ddd6b3a094ffe3f04c097
Y-Moshe <moshe.nehemiah@gmail.com>
2022-11-30 13:56:28 +0200

    font-awesome

---
+++ src/font-awesome.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faAirbnb } from '@fortawesome/free-brands-svg-icons'
+
+library.add(faAirbnb)
--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import { store } from './store/store.js'
 import './assets/styles/main.scss'
 import rootCmp from './root-cmp.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import './font-awesome'
+
 const app = createApp(rootCmp)
+
+app.component('fa-icon', FontAwesomeIcon)
 app.use(router)
 app.use(store)
 
