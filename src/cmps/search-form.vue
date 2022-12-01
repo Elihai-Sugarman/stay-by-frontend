1a0c9a5f4334ec4a09122a0cdc98972fa1165211
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-01 17:30:25 +0200

    search form improve

--- src/cmps/search-form.vue
+++ src/cmps/search-form.vue
@@ -1,30 +1,48 @@
 <template>
-  <form @submit.prevent="" class="search-form">
-    <label>
-      Where?
-      <input type="text">
-    </label>
-    <label>
-      Where?
-      <input type="date">
-    </label>
-    <label>
-      Where?
-      <input type="date">
-    </label>
-    <label>
-      Where?
-      <select>
-        <option value="">1</option>
-        <option value="">2</option>
-        <option value="">3</option>
-      </select>
-    </label>
+  <el-form class="search-form" :model="form">
+    <div class="container">
+      <label>
+        <!-- <el-input v-model="form.where" placeholder="Where" size="large" /> -->
+        <el-autocomplete
+          v-model="form.where"
+          :fetch-suggestions="searchLocations"
+          placeholder="Where"
+          @select="handleSelect"
+          clearable
+        />
+      </label>
 
-    <button type="button">
-      <icon icon-type="search" />
-    </button>
-  </form>
+      <span class="splitter"></span>
+  
+      <div>
+        <el-date-picker
+            v-model="form.checkDates"
+            type="daterange"
+            start-placeholder="Check in"
+            end-placeholder="Check out"
+            size="large"
+          />
+      </div>
+
+      <span class="splitter"></span>
+  
+      <label>
+        Who
+        <select>
+          <option value="">1</option>
+          <option value="">2</option>
+          <option value="">3</option>
+        </select>
+      </label>
+  
+      <div>
+        <button type="button" @click="handleSearch">
+          <icon icon-type="search" />
+        </button>
+
+      </div>
+    </div>
+  </el-form>
 </template>
 
 <script>
@@ -33,6 +51,34 @@ import icon from './icon-cmp.vue'
 export default {
   components: {
     icon
+  },
+  data() {
+    return {
+      form: {
+        where: '',
+        checkDates: [],
+        guests: []
+      }
+    }
+  },
+  methods: {
+    handleSearch() {
+      console.log('this.form', this.form)
+    },
+    searchLocations(queryString, cb) {
+      const regex = new RegExp(queryString, 'i')
+      const filteredLocs = this.AllLocations.filter(locString => regex.test(locString))
+
+      cb(filteredLocs.map(locStr => ({ value: locStr })))
+    },
+    handleSelect(item) {
+      this.form.where = item
+    }
+  },
+  computed: {
+    AllLocations() {
+      return this.$store.getters.locations
+    }
   }
 }
 </script>
