e88ea3513a6d2344b7864cf752c9667cced1454a
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-02 02:16:46 +0200

    define stay-header layout

--- src/cmps/stay/stay-header.vue
+++ src/cmps/stay/stay-header.vue
@@ -1,14 +1,22 @@
 <template>
-    <section class="stay-header flex row">
-        <stay-label v-for="label in labels" :label="label" @filter="filter" />
+    <section class="stay-header">
+        <div class="labels-container">
+            <stay-label v-for="label in labels" :label="label" @filter="filter" />
+        </div>
+        <div class="flex justify-center align-center h-100">
+            <stay-filter />
+        </div>
     </section>
 </template>
 
 <script>
+import stayFilter from './stay-filter.vue'
+
 import stayLabel from './stay-label.vue';
     export default {
         components:{
             stayLabel,
+            stayFilter
         },
         computed: {
             labels(){
