49cc8a66712be748098c15b2991245cb04485387
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-03 20:50:35 +0200

    filters transitions

--- src/cmps/search-bar.vue
+++ src/cmps/search-bar.vue
@@ -1,27 +1,33 @@
 <template>
   <section class="search-bar" @click="$emit('click')">
-    <div v-if="!open" class="search-preview-container">
-      <div class="search-preview" v-if="isInHomePage">
-        <template v-if="true">
+    <Transition name="slide-down">
+      <div v-if="!open" class="search-preview-container">
+  
+        <div class="search-preview" v-if="!isInStayDetails">
           <button type="button" @click.stop="$emit('anyWhere')">Anywhere</button>
           <span class="splitter"></span>
           <button type="button" @click.stop="$emit('anyWeek')">Any week</button>
           <span class="splitter"></span>
           <button type="button" class="guests-btn" @click.stop="$emit('addGuests')">Add guests</button>
           <button type="button" class="search-btn" @click="$emit('click')"><icon icon-type="search" /></button>
-        </template>
+        </div>
+  
+        <div class="search-preview stay-details-search" v-else>
+          <div>Start your search</div>
+          <button type="button" class="search-btn" @click="$emit('click')"><icon icon-type="search" /></button>
+        </div>
+  
       </div>
-      <div class="search-preview" v-else>
-        <input type="text" placeholder="Start your search">
-        <button type="button"><icon icon-type="search" /></button>
-      </div>
-    </div>
+    </Transition>
 
-    <search-form v-else />
+    <Transition name="fade-in">
+      <search-form v-if="open" />
+    </Transition>
   </section>
 </template>
 
 <script>
+import { Transition } from 'vue'
 import icon from './icon-cmp.vue'
 import searchForm from './search-form.vue'
 
@@ -31,13 +37,13 @@ export default {
   components: {
     icon,
     searchForm
-  },
+},
   data() {
     return {}
   },
   computed: {
-    isInHomePage() {
-      return !this.$route.params.id
+    isInStayDetails() {
+      return this.$route.name === 'stay-details'
     }
   }
 }
