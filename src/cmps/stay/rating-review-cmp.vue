7eaeab8799b4ec5484edb96a69c9aa9a028b9554
Miriam Smith <miriamzeff@gmail.com>
2022-12-03 22:00:23 +0200

    added rating review cmp

---
+++ src/cmps/stay/rating-review-cmp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div v-if="reviews" class="rating-review flex">
+    <span class="avg-rating flex font-md">
+      <icon-cmp iconType="star" />
+      {{ avgRating }}
+    </span>
+    <span>•</span>
+    <span class="rev-count link font-md"> {{ countReviews }} reviews</span>
+  </div>
+</template>
+<script>
+import iconCmp from '../icon-cmp.vue'
+
+export default {
+  name: 'rating-review',
+  props: {
+    reviews: Array,
+  },
+  data() {
+    return {
+      
+    }
+  },
+  computed: {
+    avgRating() {
+      return +(
+        this.reviews.reduce((acc, currRev) => acc + currRev.rate, 0) /
+        this.countReviews
+      ).toFixed(2)
+    },
+    countReviews() {
+      return this.reviews.length
+    },
+  },
+  components: {
+    iconCmp,
+  },
+}
+</script>
