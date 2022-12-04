003096627fe67e0bf44afb21ee6b3939549881c6
Miriam Smith <miriamzeff@gmail.com>
2022-12-04 18:32:05 +0200

    added all reviews modal

---
+++ src/cmps/stay/all-reviews-modal.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="reviews" class="all-reviews-modal">
+    <div class="modal-content">
+      <span class="close">&times;</span>
+      <h1 class="flex subheading">
+        <ratingReview :reviews="reviews" />
+      </h1>
+      <div class="reviews-container">
+        <review-cmp :reviews="reviews" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import ratingReview from './rating-review-cmp.vue'
+import reviewCmp from './review-cmp.vue'
+
+export default {
+  name: 'all-reviews-modal',
+  props: {
+    reviews: Array,
+  },
+  data() {
+    return {}
+  },
+  components: {
+    ratingReview,
+    reviewCmp,
+  },
+}
+</script>
