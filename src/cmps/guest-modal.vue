7c5a1fe76496c8d49a9497c0282948a2f1e7b6e1
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-04 17:39:26 +0200

    guest-modal

---
+++ src/cmps/guest-modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="guest-modal" v-if="open" @click.stop>
+    <div v-for="[title, guest] in Object.entries(options)" :id="title" class="guest-option">
+      <div class="description">
+        <div class="title">{{ title }}</div>
+        <div class="subtitle">{{ guest.subtitle }}</div>
+      </div>
+
+      <div class="guest-actions">
+        <button type="button" @click="$emit('decrement', title)" :disabled="!guest.capacity">
+          <icon-cmp icon-type="minus" />
+        </button>
+        <span>{{ guest.capacity }}</span>
+        <button type="button" @click="$emit('increment', title)" :disabled="(guest.capacity === guest.maxCapacity)">
+          <icon-cmp icon-type="plus" />
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  // options: {
+  //   Adults: {
+  //     subtitle: 'Ages 13 or above',
+  //     capacity: 0
+  //   },
+  //   Children: {
+  //     subtitle: 'Ages 2–12',
+  //     capacity: 0
+  //   },
+  //   Infants: {
+  //     subtitle: 'Under 2',
+  //     capacity: 0
+  //   },
+  //   Pets: {
+  //     subtitle: 'Bringing a service animal?',
+  //     capacity: 0
+  //   }
+  // }
+  props: { options: Object, open: Boolean },
+  emits: ['increment', 'decrement']
+}
+</script>
