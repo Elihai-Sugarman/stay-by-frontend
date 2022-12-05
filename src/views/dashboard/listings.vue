c438244c3925b32dc496d65ad3feed736c2b04a3
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-05 23:48:32 +0200

    dashboard listings

--- src/views/dashboard/listings.vue
+++ src/views/dashboard/listings.vue
@@ -1,7 +1,75 @@
 <template>
-  listings
+  <section class="dashboard-listings">
+    <div class="listing-title">
+      <div>{{ tableData.length }} Listings</div>
+      <el-button plain><icon-cmp icon-type="plus" /> Create listing</el-button>
+    </div>
+    <el-table :data="tableData" height="200">
+
+      <el-table-column label="LISTING" width="250">
+        <template #default="scope">
+          <div class="listing-preview">
+            <img :src="scope.row.imgUrls[0]" alt="listing preview">
+            <h3>{{ scope.row.name }}</h3>
+          </div>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="TODO">
+        <template #default>
+          <el-button plain>Update</el-button>
+        </template>
+      </el-table-column>
+
+      <el-table-column prop="capacity" label="CAPACITY" width="120" align="center" sortable />
+
+      <el-table-column prop="bathrooms" label="ROOMS" align="center" sortable />
+      
+      <el-table-column prop="bedrooms" label="BEDROOMS" width="140" align="center" sortable />
+
+      <el-table-column prop="price" :formatter="formatCurrency" label="PRICE" sortable />
+
+      <el-table-column :formatter="formatLocation" label="LOCATION" align="center" />      
+
+      <el-table-column prop="createdAt" width="150" label="DATE ADDED" />
+    </el-table>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  created() {
+    this.$store.dispatch({ type: 'loadStays' })
+    .then(() => this.loadListingsData())
+    
+  },
+  computed: {
+    user() {
+      return this.$store.getters.loggedinUser
+    },
+    stays() {
+      return this.$store.getters.stays
+    }
+  },
+  methods: {
+    loadListingsData() {
+      const data = this.stays.filter(stay => stay.host._id === this.user._id)
+      this.tableData = data
+    },
+    formatLocation({ address }) {
+      return `${address.city}, ${address.country}`
+    },
+    formatCurrency({ price }) {
+      return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      }).format(price)
+    }
+  }
+}
 </script>
