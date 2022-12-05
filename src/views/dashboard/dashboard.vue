05db09560db3d27f0f2af5952f9023e523d883bb
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-05 20:11:14 +0200

    basic dasboard nav / layout

--- src/views/dashboard/dashboard.vue
+++ src/views/dashboard/dashboard.vue
@@ -1,6 +1,23 @@
 <template>
-  DASHBOARD
-  <router-view></router-view>
+  <section class="dashboard">
+    <aside class="side-nav">
+      <ul class="clean-list">
+        <router-link to="/dashboard/edit">
+          <li>Add stay</li>
+        </router-link>
+        <router-link to="/dashboard/listings">
+          <li>My listings</li>
+        </router-link>
+        <router-link to="/dashboard/order">
+          <li>Orders</li>
+        </router-link>
+      </ul>
+    </aside>
+
+    <section class="dashboard-content">
+      <router-view></router-view>
+    </section>
+  </section>
 </template>
 
 <script>
