c018257ab88ce068cb9e283f6da9d0175ccdfae8
Elihai Sugarman <elihaisugarman@gmail.com>
2022-12-07 11:38:10 +0200

    trips update

--- src/views/dashboard/order.vue
+++ src/views/dashboard/order.vue
@@ -1,25 +1,29 @@
 <template>
   <section class="dashboard-listings">
     <div class="listing-title">
-      <div>{{ orders.length }} Reservation<span v-if="(orders.length>1)">s</span></div>
+      <div>{{ orders.length }} Reservation<span v-if="(orders.length > 1)">s</span></div>
     </div>
     <el-table :data="tableData" align="center">
 
-      <el-table-column fixed label="Guest" min-width="100" prop="renter.fullname" align="center">
+      <el-table-column fixed label="Guest" min-width="150" prop="renter.fullname" align="center">
         <template #default="scope">
-          <h3 class="renter-fullname">{{ scope.row.renter.fullname }}</h3>
+          <h3 class="renter-fullname handle-overflow">{{ scope.row.renter.fullname }}</h3>
         </template>
       </el-table-column>
       
-      <el-table-column prop="startDate" min-width="100" label="Check-in" align="center" sortable/>
+      <el-table-column prop="startDate" min-width="100" :formatter="getFormattedStartDate"  label="Check-in" align="center" sortable/>
 
-      <el-table-column prop="endDate" min-width="100" label="Checkout" align="center" sortable/>
+      <el-table-column prop="endDate" min-width="100" :formatter="getFormattedEndDate"  label="Checkout" align="center" sortable/>
       
-      <el-table-column prop="createdAt" min-width="100" label="Booked" align="center" sortable/>
+      <el-table-column prop="createdAt" min-width="100" :formatter="getFormattedBookedDate"  label="Booked" align="center" sortable/>
       
-      <el-table-column prop="stay.name" min-width="250" label="Listing" align="center"/>
+      <el-table-column prop="stay.name" min-width="250" label="Listing" align="center">
+        <template #default="scope">
+          <span class="handle-overflow">{{ scope.row.stay.name }}</span>
+        </template>
+      </el-table-column>
       
-      <el-table-column prop="totalPrice" min-width="100" :formatter="formatCurrency" label="Toal Payout" align="center" sortable />
+      <el-table-column prop="totalPrice" min-width="110" :formatter="formatCurrency" label="Total Payout" align="center" sortable />
       
       <el-table-column label="Status" align="center" min-width="100" sortable>
         <template #default="scope">
@@ -43,6 +47,7 @@
 </template>
 
 <script>
+import * as moment from 'moment'
 import { capitalize } from 'lodash'
 import { utilService } from '../../services/util.service'
 
@@ -60,14 +65,14 @@ export default {
   computed: {
     orders(){
       const filteredOreders = this.$store.getters.orders
+      .filter(order => {
+        return order.hostId === this.$store.getters.loggedinUser._id
+      })
       return filteredOreders
     },
     user() {
       return this.$store.getters.loggedinUser
     },
-    orders() {
-      return this.$store.getters.orders
-    }
   },
   methods: {
     getStatusClass({ status }) {
@@ -106,7 +111,17 @@ export default {
       this.colorClass = order.status === 'approved' ? 'color-green' : 'color-red'
       const newOrder = utilService.deepCopy(order)
       this.$store.dispatch({type: 'updateOrder', order: newOrder})
-    }
+    },
+    getFormattedStartDate({ startDate }) {
+      return moment(startDate).format('MM/DD/YYYY')
+    },
+    getFormattedEndDate({ endDate }) {
+      return moment(endDate).format('MM/DD/YYYY')
+    },
+    getFormattedBookedDate({ createdAt }) {
+      return moment(createdAt).format('MM/DD/YYYY')
+    },
   }
+  
 }
 </script>
